cfab110fb96a131f03e895aefd64b85d7b6f314a
unknown <iurii.izotov@gmail.com>
2022-08-29 20:52:19 +0300

    add: useState

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+
+export const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(2);
+    const categories=['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index);
+    }
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((category,index)=> {
+                    return (
+                        <li onClick={() => onClickCategory(index)} className={activeIndex === index ? 'active' : ''}>
+                            {category}
+                        </li>
+                    )
+                }
+                )
+                }
+            </ul>
+        </div>
+    )
+}
