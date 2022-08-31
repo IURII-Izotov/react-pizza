53be1229b11ce4afd54d4ea21774486c9dee1009
unknown <iurii.izotov@gmail.com>
2022-08-31 21:22:35 +0300

    add: async recuest for arr pizzas

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import {Sort} from "./components/Sort";
 import {Header} from "./components/Header";
 import {Categories} from "./components/Categories";
 import {PizzaBlock} from "./components/PizzaBlock";
-import pizzas from './assets/pizza.json'
+
+import {useEffect, useState} from "react";
 function App() {
+    const [items,setItems]=useState([])
+    useEffect(()=>{
+        fetch('https://630e6210109c16b9abfa526d.mockapi.io/items').then(res=>{
+            return res.json()
+        }).then(json=>{
+            setItems(json);
+        })
+    },[])
     return (
         <div className="wrapper">
             <Header/>
@@ -17,7 +26,7 @@ function App() {
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
                         {
-                            pizzas.map((obj)=>{
+                            items.map((obj)=>{
                                 return <PizzaBlock key={obj.id} {...obj}/>
                             })
                         }
